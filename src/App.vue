c7946a91ba16155e75b03683dc30db2f864e6ed2
Craig Barratt <craigtbarratt@gmail.com>
2020-04-23 00:14:45 +0100

    adds global styling

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template lang="html">
-  <div>
-    <h1>Rick & Morty</h1>
-    <character-list :characters="characters" v-model="selectedCharacter"></character-list>
-    <character-detail v-if="selectedCharacter" :character="selectedCharacter"></character-detail>
-  </div>
+    <div>
+      <p class="heading">Rick <span class="heading-and">and</span> Morty</p>
+      <character-list :characters="characters" v-model="selectedCharacter"></character-list>
+      <character-detail v-if="selectedCharacter" :character="selectedCharacter"></character-detail>
+    </div>
 </template>
 
 <script>
@@ -38,4 +38,35 @@ export default {
 </script>
 
 <style lang="css" scoped>
+
+  .heading {
+    font-family: 'RickMortyFont';
+    color: #41B4C7;
+    text-shadow: -1px -1px 0 #6BA230, 1px -1px 0 #6BA230, -1px 1px 0 #6BA230, 1px 1px 0 #6BA230;
+    font-size: 100px;
+    text-align: center;
+    padding: 25px;
+    margin: 0;
+  }
+
+  .heading-and {
+    font-size: 50px;
+  }
+
+</style>
+
+<style>
+body {
+  background-image: url('./assets/background.png');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 100vw;
+  height: 100vh;
+}
+
+@font-face {
+  font-family: 'RickMortyFont';
+  src: url('./assets/fonts/font.ttf');
+}
 </style>
